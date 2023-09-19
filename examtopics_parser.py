ba68a938893b7ae1199ed5105d330b1628fdc1ba
CodeBreaker44 <89404773+CodeBreaker44@users.noreply.github.com>
2023-09-19 20:48:29 +0300

    added more user_agent

--- examtopics_parser.py
+++ examtopics_parser.py
@@ -26,7 +26,7 @@ start_question = int(input("Enter start question: "))
 end_question = int(input("Enter end question: "))
 
 user_agents = [
-    #  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
     # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0",
 ]
